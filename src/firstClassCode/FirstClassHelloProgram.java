27ec6f7a04b9d3ce0923530c285318ff2221cdca
surjeet2021 <surjeet101@gmail.com>
2021-09-30 08:24:46 +0530

    1 commit

---
+++ src/firstClassCode/FirstClassHelloProgram.java
@@ -0,0 +1,19 @@
+package firstClassCode;
+
+public class FirstClassHelloProgram {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Good");
+		System.out.println("Morning");
+		
+		System.out.print("Good");
+		System.out.print("Morning");
+		
+		
+		
+
+	}
+
+}
