5514ce9332d8858151d26cc3e4dd364487c38725
surjeet2021 <surjeet101@gmail.com>
2021-10-02 08:25:18 +0530

    1 program commit

--- src/firstClassCode/FirstClassHelloProgram.java
+++ src/firstClassCode/FirstClassHelloProgram.java
@@ -4,9 +4,19 @@ public class FirstClassHelloProgram {
 
 	public static void main(String[] args) {
 		
+		String value="Day";
 		
-		System.out.println("Good");
-		System.out.println("Morning");
+		int value1=1;
+	     value1=++value1;
+		
+		System.out.println(2+value1);
+		
+			
+		
+		System.out.println("Good"+value);
+		System.out.println("Morning"); 
+		
+		
 		
 		System.out.print("Good");
 		System.out.print("Morning");
